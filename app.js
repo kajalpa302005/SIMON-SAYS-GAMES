951fb8f225354289c9ca0c92b88bdfe8184e0f56
KAJAL PANDEY <166059220+kajalpa302005@users.noreply.github.com>
2025-05-08 16:44:52 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -15,25 +15,76 @@ document.addEventListener("keypress", function(){
     }
 
 });
-function btnFlash(btn){
+
+function gameFlash(btn){
     btn.classList.add("flash");
     setTimeout(function () { 
         btn.classList.remove("flash");
-    } , 1000);
-
+    } , 250);
 }
+
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function () { 
+        btn.classList.remove("userflash");
+    } , 250);
+}
+
 function levelUp() {
+    userSeq =[];
     level++;
     h2.innerText=`level ${level}`;
 
-    let randIdx=Math.floor(Math.random() +3);
+    let randIdx=Math.floor(Math.random() *3);
     let randColor= btn[randIdx];
     let randBtn= document.querySelector(`.${randColor} `);
-    S
-    console.log(randIdx);
-    console.log(randColor);
-    console.log(randBtn);
-    btnFlash(randBtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    
+    //console.log(randIdx);
+    //console.log(randColor);
+    //console.log(randBtn);
+    gameFlash(randBtn);
 
 
+}
+
+
+function checkAns(idx) {
+
+    if (userSeq[idx]===gameSeq[idx]){
+     if (userFlash.Seq.length == gameSeq.length){
+        setTimeout(levelUp, 1000);
+     }
+    } else{
+        h2.innerHTML=`Game Over! Your score was<b> ${level}</b>  <br>Press any key to start.`;
+        document.querySelector("body").style.backgroundcolor="red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundcolor="white";
+        }, 150);
+        reset();
+
+  
+    }
+}
+function btnPress() {
+ let btn= this;
+ userFlash(btn);
+
+ userColor = btn.getAttribute("id");
+ userSeq.push(userColor);
+
+ checkAns(userSeq.length-1);
+}
+
+let allbtn=document.querySelectorAll(".btn")
+for  ( btn of allbtn) {
+    btn.addEventListener("click", btnPress);
+}
+
+function reset(){
+    started =false;
+    gameSeq=[];
+    userSeq=[];
+    level=0;
 }
